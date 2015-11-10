758adb58c019639edc86af48036dc45aae40c7c7
Clemens Wagner <wagner.clemens@gmx.de>
2015-11-10 12:10:54 +0100

    Aufgabe c bissel was gemacht

--- muster_3/main.py
+++ muster_3/main.py
@@ -23,43 +23,54 @@ testing = load_csv('data/chickwts_testing.csv', ',', lambda x: int(x))
 values = range(0, 550, 1)
 p = []
 max = 6
+priori = []
+mu = []
+var = []
 
 print("\\begin{tabular}[c]{|c|c|c|}")
 print("\\hline")
-print("Futterklasse & Erwartungswert ($\\mu$) & Varianz ($\\sigma^2$) \\")
+print("Futterklasse & Erwartungswert ($\\mu$) & Varianz ($\\sigma^2$) & A-Priori\\")
 print("\\hline")
+
+run = 'b'
 
 for i in range(max):
     weight = np.array(list(map(lambda x: x[1], filter(lambda x: x[2] == i, training))))
     n = len(weight)
-    µ = np.sum(weight) / n
-    v = np.sum(np.power(np.subtract(µ, weight), 2.0)) / n
+    m = np.sum(weight) / n
+    v = np.sum(np.power(np.subtract(m, weight), 2.0)) / n
 
     k = 1.0 / math.sqrt(2 * math.pi * v)
-    w = list(map(lambda x: k * math.exp(-1/2 * ((x - µ) ** 2)/v), values))
+    w = list(map(lambda x: k * math.exp(-1/2 * ((x - m) ** 2)/v), values))
+    prior = n / len(training)
 
     p.append(w)
-    plt.plot(values, w, label=("Futterklasse %s" % i))
-    print("%s & %0.2f & %0.2f \\\\" % (i, µ, v))
+    if run == 'a':
+        plt.plot(values, w, label=("Futterklasse %s" % i))
+    print("%s & %0.2f & %0.2f & %0.2f \\\\" % (i, m, v, prior))
+    mu.append(m)
+    priori.append(prior)
+    var.append(v)
     print("\\hline")
 
 print("\\end{tabular}")
 
-plt.title("Aufgabe 1b - Wahrscheinlichkeitsdichte")
-plt.ylabel('Wahrscheinlichkeit p(x)')
-plt.xlabel('Gewicht (x)')
-plt.legend(loc='upper right', shadow=False, fontsize='x-small')
-plt.show()
+if run == 'a':
+    plt.title("Aufgabe 1b - Wahrscheinlichkeitsdichte")
+    plt.ylabel('Wahrscheinlichkeit p(x)')
+    plt.xlabel('Gewicht (x)')
+    plt.legend(loc='upper right', shadow=False, fontsize='x-small')
+    plt.show()
 
 
 for test in testing:
     weight = test[1]
-    best = 0
+
     k = None
-    for i in range(max):
-        current = p[i][weight]
-        if best < current:
-            best = current
-            k = i
-    print("weight: %s, class: %s" % (weight, k))
+    ck = range(max)
+    pw = len(list(filter(lambda x: x[1] == weight, testing)))
+
+    pck = [(priori[k] *  p[k][weight]) / pw for k in ck]
+
+
 
