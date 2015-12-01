0f43dc1dfb8317762d4ad86232cc344fb60ee59e
Clemens Wagner <wagner.clemens@gmx.de>
2015-12-01 11:57:29 +0100

    gesäubert

--- muster_5/main.py
+++ muster_5/main.py
@@ -1,26 +1,51 @@
 import io
 import numpy as np
-import math
-import random
 import matplotlib.pyplot as plt
-import scipy.stats as stat
+from mpl_toolkits.mplot3d import axes3d
 
 
-def load_csv(path, cast=lambda x: float(x), max_width = 0):
+def load_csv(path, char = ',', cast = lambda x: float(x)):
+    max_width = 0
     f = io.open(path, 'r')
     line = f.readline()
     data = []
 
-    while (line):
-        values = np.array(line.split())
+    while(line):
+        values = line.strip(' \t\n\r').split(char)
         if (max_width == 0):
             max_width = len(values)
-        data.append(np.array([cast(x.strip(' \t\n\r')) for x in values[:max_width]]))
+        data.append(np.array([cast(x) for x in values[:max_width]]))
         line = f.readline()
 
-    return np.array(data)
+    return data
 
 
-fish = load_csv("data/fish.txt", "\s+", lambda x: int(x))
+fishData = load_csv('fish.txt', cast = lambda x: int(x))
+fishLength = np.array(fishData)[:, 3]
+fish1AgeTemp = []
+for i in range(len(fishData)):
+    fish1AgeTemp.append([1, fishData[i][1], fishData[i][2]])
 
-print ("fish: %s" % fish)
+x = np.dot(np.array(fish1AgeTemp).T, np.array(fish1AgeTemp))
+beta = np.dot(np.dot(np.linalg.inv(x), np.array(fish1AgeTemp).T), np.array(fishLength))
+print (beta)
+
+for i in range(len(fishData)):
+    print ('geschaetzt: %i exakt: %i' % ((beta[0] + beta[1] * fish1AgeTemp[i][1] + beta[2] * fish1AgeTemp[i][2], fishLength[i])))
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+xs = np.array(fish1AgeTemp)[:, 1]
+ys = np.array(fish1AgeTemp)[:, 2]
+ax.set_xlabel('Alter')
+ax.set_ylabel('Temperatur')
+ax.set_zlabel('Laenge')
+
+ax.scatter(xs, ys, fishLength, color = 'r', marker = 'o') #exakt
+ax.scatter(xs, ys, (np.add(np.add(beta[0], np.multiply(beta[1], np.array(fish1AgeTemp)[:, 1])), np.multiply(beta[2], np.array(fish1AgeTemp)[:, 2]))), color = 'b', marker = 'o') #lineare Regression
+
+ax.plot_surface(np.array(fish1AgeTemp)[:, 1], np.array(fish1AgeTemp)[:, 2], np.add(np.add(beta[0], np.multiply(beta[1], np.array(fish1AgeTemp)[:, 1])), np.multiply(beta[2], np.array(fish1AgeTemp)[:, 2])), cmap='summer')
+plt.show()
+
+
+
